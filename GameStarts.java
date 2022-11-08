e694063d0cd04455fcc3b3572dd1ba32a11b8044
charanmogili <115568271+charanmogili@users.noreply.github.com>
2022-11-08 10:59:27 +0530

    Add files via upload

---
+++ GameStarts.java
@@ -0,0 +1,12 @@
+
+public class GameStarts {
+
+	public static void main(String[] args) {
+		Umpire u=new Umpire();
+		u.CollectNoFromGuesser();
+		u.CollectNoFromPlayer();
+		u.Compare();
+
+	}
+
+}
